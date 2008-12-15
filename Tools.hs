e588dd277cd8ea9bcda9017da55a9d34e9b5f90d
Neun <daniel.neun@gmx.de>
2008-12-15 16:45:15 +0100

    Added some haddock documentation

--- Tools.hs
+++ Tools.hs
@@ -2,17 +2,28 @@ module Tools where
 
 import Random
 
-willMutate :: Integer -> IO Bool
+-- |Decide whether it`s time for a mutation
+willMutate :: Integer  -- ^ Mutation rate 
+           -> IO Bool  -- ^ True: Mutate
 willMutate mutationRate = do k <- getRandomNumber 0 mutationRate
                              return (k == 1)
 
-getRandomNumber :: Random a => a -> a -> IO a
+-- |Get a random number between min and max
+getRandomNumber :: Random a => 
+                   a     -- ^ Minimum
+                -> a     -- ^ Maximum
+                -> IO a  -- ^ Random number action
 getRandomNumber x y = getStdRandom (randomR (x,y))
 
-maybeMutate :: Integer -> IO a -> a -> IO a
+-- |Perform a mutation action when it`s time to do so
+maybeMutate :: Integer  -- ^ Mutation rate
+            -> IO a     -- ^ Mutation action
+            -> a        -- ^ Unchanged value to pass through
+            -> IO a     -- ^ Composed action
 maybeMutate rate action unchanged = do mutate <- willMutate rate
                                        if mutate then action else return unchanged
 
+-- |Instances of Mutable can mutate their DNA
 class Mutable a where
     mutate :: a -> IO a
 

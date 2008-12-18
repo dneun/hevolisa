5c8c0155124fa3774b675d4fb21fe8c6f7c2518d
Daniel Neun <daniel@localhost.(none)>
2008-12-18 19:49:33 +0100

    changed Main

--- Main.hs
+++ Main.hs
@@ -1,8 +1,13 @@
 module Main where
 
 import DnaDrawing
+import Evolution
 import Tools
 
+-- main :: IO ()
+-- main = do drawing <- mapseq initDrawing $ replicate 100000 mutate
+--           putStrLn $ show drawing
+
 main :: IO ()
-main = do drawing <- mapseq initDrawing $ replicate 100000 mutate
-          putStrLn $ show drawing
+main = mapseq (initContext [[]]) (replicate 10 mutate) >>= \c -> 
+       (putStrLn . show) c

0e9ca087f167b581ddb092587fc53068c65e7325
Neun <daniel.neun@gmx.de>
2008-12-19 18:06:16 +0100

    changed Main

--- Main.hs
+++ Main.hs
@@ -1,13 +1,16 @@
 module Main where
 
+import DnaPoint
+import DnaBrush
+import DnaPolygon
 import DnaDrawing
 import Evolution
 import Tools
 
--- main :: IO ()
--- main = do drawing <- mapseq initDrawing $ replicate 100000 mutate
---           putStrLn $ show drawing
-
 main :: IO ()
-main = mapseq (initContext [[]]) (replicate 10 mutate) >>= \c -> 
-       (putStrLn . show) c
+main = do (mapseq randomInit $ replicate 1000 mutate :: IO DnaDrawing) >>= putStrLn . show
+          (randomInit >>= mutate >>= mutate :: IO DnaPoint) >>= putStrLn . show
+          (randomInit >>= mutate >>= mutate :: IO DnaBrush) >>= putStrLn . show
+          (randomInit >>= mutate >>= mutate :: IO DnaPolygon) >>= putStrLn . show
+          (randomInit >>= mutate >>= mutate :: IO DnaDrawing) >>= putStrLn . show
+          

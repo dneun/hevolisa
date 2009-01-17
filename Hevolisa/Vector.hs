136fdfdcfcc916785b1ddf5a9634d08ed8978e4a
Daniel Neun <daniel@localhost.(none)>
2009-01-17 15:21:24 +0100

    implementation of the error function with dph

---
+++ Hevolisa/Vector.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE PArr #-}
+{-# OPTIONS -fvectorise #-}
+module Hevolisa.Vector where
+
+import qualified Prelude as P
+import Data.Array.Parallel.Prelude
+import Data.Array.Parallel.Prelude.Int
+
+error :: [:Int:] -> [:Int:] -> Int
+error c1 c2 = sumP (zipWithP (\x y -> (x - y)*(x - y)) c1 c2)
+
+error_wrapper :: PArray Int -> PArray Int -> Int
+{-# NOINLINE error_wrapper #-}
+error_wrapper c1 c2 = error (fromPArrayP c1) (fromPArrayP c2)

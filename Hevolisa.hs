0ac0c22e5dc626fab8c43b81516befc0becab0de
Daniel Neun <daniel@localhost.(none)>
2009-02-13 22:35:36 +0100

    command line options (cherry picked from commit fdd9af443858e660410a276cdfb88158a0325ecf)

--- Hevolisa.hs
+++ Hevolisa.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE PatternGuards #-}
 --
 -- Module      : Hevolisa
 -- Copyright   : (c) Daniel Neun 2008
@@ -9,7 +10,34 @@
 module Main where
 
 import Hevolisa.Evolution (start)
+import System.Console.GetOpt
+import System.Environment (getArgs)
+import System.Exit (ExitCode(..),exitWith)
+import System.IO (hPutStrLn,stderr,stdout)
+
+data Flag = Help | F FilePath
+            deriving Eq
+
+options :: [OptDescr Flag]
+options = [ Option ['h'] ["help"] (NoArg Help)
+                   "Show this help message",
+            Option ['f'] ["file"] (ReqArg (\s -> F s) "FILE")
+                   "Path to source image file" ]
 
 
 main :: IO ()
-main = start "mona_lisa_crop.png" >> return ()
+main = parseArgs >> return ()
+
+parseArgs = do
+  argv <- getArgs
+  case parse argv of
+    (opts,files,[])
+        | Help `elem` opts                 -> help
+        | [F path] <- filter (/=Help) opts -> start path
+    (_,_,errs)                             -> die errs
+  where parse argv = getOpt Permute options argv
+        header = "Usage: hevolisa -f file"
+        info = usageInfo header options
+        dump = hPutStrLn stderr
+        die errs = dump (concat errs ++ info) >> exitWith (ExitFailure 1)
+        help = dump info                      >> exitWith ExitSuccess
